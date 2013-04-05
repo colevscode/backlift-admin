836e4f6f286fb8216ba814b3dba0c41f074ffa1b
laurihy <lauri.hynynen@gmail.com>
2013-04-05 03:15:22 +0300

    init

---
+++ scripts/admin.js
@@ -0,0 +1,22 @@
+/*
+* Initiates the admin app, once all necessary files have been loaded
+*/
+
+$(function(){
+
+  App.helpers.checkLogin(function(){
+    // not logged in
+    window.location = '/admin/login.html?_next=/admin';
+  }, function(){
+
+    //logged in, run app
+    $('#app-username').text(App.current_user.username);
+    $('#app-appname').text(App.adminData._id);
+
+    new App.router();
+    Backbone.history.start({pushState: false, root: 'dashboard'}); 
+
+    $('body').removeClass('hidden');
+  });
+   
+});
